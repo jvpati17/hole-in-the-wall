8aa6caf30c173820cc56819924720240e1f719d7
Tifanie Thomas <tifanie.n.thomas@gmail.com>
2023-09-15 15:48:20 -0500

    add login_required to functions. add logout button to nav bar. signup template created

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
+@login_required 
 def about(request):
     return render(request, 'about.html')
 
@@ -22,3 +24,9 @@ def signup(request):
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
+
+# @login_required 
+# def reviews_index(request):
+#   reviews = Review.objects.filter(user=request.user)
+#   # You could also retrieve the logged in user's reviews
+#   return render(request, 'reviews/index.html', { 'reviews': reviews })
