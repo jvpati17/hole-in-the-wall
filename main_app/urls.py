ef94b08e0d5fa3e3e248dff31862adfc58746c34
Rebecca <mendez_elizabeth88@hotmail.com>
2023-09-16 11:15:32 -0700

    add detail page and edit index

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('users/signup/', views.signup, name='signup'),
     path('restaurants/', views.restaurants_index, name='index'),
     path('restaurants/<int:restaurant_id>/', views.restaurants_detail, name='detail'),
+    path('restaurants/create/', views.RestaurantCreate.as_view(), name='restaurants_create'),
+    path('restaurants/<int:restaurant_id>/update/', views.RestaurantUpdate.as_view(), name='restaurants_update')
 ]
