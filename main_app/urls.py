22060be3f9c79ef2ac116329fc055666060e8d4b
jvpati17 <139751449+jvpati17@users.noreply.github.com>
2023-09-16 19:42:36 -0700

    edit page + options

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('restaurants/', views.restaurants_index, name='index'),
     path('restaurants/<int:restaurant_id>/', views.restaurants_detail, name='detail'),
     path('restaurants/create/', views.RestaurantCreate.as_view(), name='restaurants_create'),
-    path('restaurants/<int:restaurant_id>/update/', views.RestaurantUpdate.as_view(), name='restaurants_update')
+    path('restaurants/<int:pk>/update/', views.RestaurantUpdate.as_view(), name='restaurants_update')
 ]
